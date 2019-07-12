409294afe898b4cb94e8869d0c8ecdb8856e8ba1
coderfocus <songwenjie830@gmail.com>
2019-07-12 16:44:55 +0800

    feat(spring-cloud-alibaba): 新增服务提供者

---
+++ spring-cloud-alibaba-nacos-provider/src/main/java/com/coderfocus/spring/cloud/alibaba/nacos/provider/Controller/NacosProviderController.java
@@ -0,0 +1,14 @@
+package com.coderfocus.spring.cloud.alibaba.nacos.provider.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NacosProviderController {
+
+    @GetMapping("/echo/{message}")
+    public String echo(@PathVariable(value = "message") String message){
+        return "Hellp Nacos" + message;
+    }
+}
