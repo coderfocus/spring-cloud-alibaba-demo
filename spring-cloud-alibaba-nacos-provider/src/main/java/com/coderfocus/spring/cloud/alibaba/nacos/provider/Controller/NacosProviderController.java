861611829a96fb9238782087cd2453488a847838
coderfocus <songwenjie830@gmail.com>
2019-07-15 09:53:13 +0800

    feat(spring-cloud-alibaba): 新增服务消费者-feign-负载均衡

--- spring-cloud-alibaba-nacos-provider/src/main/java/com/coderfocus/spring/cloud/alibaba/nacos/provider/Controller/NacosProviderController.java
+++ spring-cloud-alibaba-nacos-provider/src/main/java/com/coderfocus/spring/cloud/alibaba/nacos/provider/Controller/NacosProviderController.java
@@ -1,14 +1,20 @@
 package com.coderfocus.spring.cloud.alibaba.nacos.provider.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.executable.ValidateOnExecution;
+
 @RestController
 public class NacosProviderController {
 
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping("/echo/{message}")
     public String echo(@PathVariable(value = "message") String message){
-        return "Hello Nacos " + message;
+        return "Hello Nacos " + message + " i am from port " + port;
     }
 }
