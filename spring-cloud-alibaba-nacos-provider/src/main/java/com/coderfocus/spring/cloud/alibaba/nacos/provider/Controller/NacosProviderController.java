f22fc79a34b9eec125651a94b406288edf5a71b0
coderfocus <songwenjie830@gmail.com>
2019-07-15 14:37:55 +0800

    feat(spring-cloud-alibaba): 新增Nacos配置中心

--- spring-cloud-alibaba-nacos-provider/src/main/java/com/coderfocus/spring/cloud/alibaba/nacos/provider/Controller/NacosProviderController.java
+++ spring-cloud-alibaba-nacos-provider/src/main/java/com/coderfocus/spring/cloud/alibaba/nacos/provider/Controller/NacosProviderController.java
@@ -1,6 +1,9 @@
 package com.coderfocus.spring.cloud.alibaba.nacos.provider.Controller;
 
+import org.springframework.beans.PropertyValue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +16,16 @@ public class NacosProviderController {
     @Value("${server.port}")
     private String port;
 
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
     @GetMapping("/echo/{message}")
     public String echo(@PathVariable(value = "message") String message){
         return "Hello Nacos " + message + " i am from port " + port;
+    }
+
+    @GetMapping("/hi")
+    public String sayHi(){
+        return "Hi " + applicationContext.getEnvironment().getProperty("user.name");
     }
 }
