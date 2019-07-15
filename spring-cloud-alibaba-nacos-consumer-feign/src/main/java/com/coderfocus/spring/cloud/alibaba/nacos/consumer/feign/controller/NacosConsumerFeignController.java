719114b02a3b922ba1a5c7221a6c00c384ed97b5
coderfocus <songwenjie830@gmail.com>
2019-07-15 13:32:35 +0800

    feat(spring-cloud-alibaba): 新增Spring Cloud Gateway网关

--- spring-cloud-alibaba-nacos-consumer-feign/src/main/java/com/coderfocus/spring/cloud/alibaba/nacos/consumer/feign/controller/NacosConsumerFeignController.java
+++ spring-cloud-alibaba-nacos-consumer-feign/src/main/java/com/coderfocus/spring/cloud/alibaba/nacos/consumer/feign/controller/NacosConsumerFeignController.java
@@ -2,6 +2,7 @@ package com.coderfocus.spring.cloud.alibaba.nacos.consumer.feign.controller;
 
 import com.coderfocus.spring.cloud.alibaba.nacos.consumer.feign.service.EchoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,11 @@ public class NacosConsumerFeignController {
     @Autowired
     private EchoService echoService;
 
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping("/echo/hi")
     public String echo(){
-        return echoService.echo("Hi feign");
+        return echoService.echo("Hi feign") + "| consumer port " + port;
     }
 }
